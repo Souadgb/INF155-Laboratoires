6992a90de5cfbba6351b0bf844824039e4607294
Souad Goubaa <souad.goubaa.1@ens.etsmtl.ca>
2025-02-16 13:29:53 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int question1() {
+    printf ("ab ovo usque ad mala");
+}
+
+int question2(double watt) {
+    printf("Nombre de watts: \n");
+    scanf("%lf", &watt);
+
+    double calories = watt*0.2388459;
+    printf("en calories: %lf", calories);
+}
+
+
+int main(void)
+{
+     question1();
+     question2();
+    //int question3();
+    //int question4();
+    //int question5();
+    //int question6();
+    //int question7();
+    //int question8();
+    //int question9();
+    //int question10();
+}
